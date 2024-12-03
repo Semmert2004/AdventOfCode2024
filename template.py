9fff64e33ab2b9d572bb676ac87bf1a4f6aa0683
semWeerheijm <s.weerheijm-1@student.utwente.nl>
2024-12-03 14:42:55 +0100

    created a template

---
+++ template.py
@@ -0,0 +1,13 @@
+import time
+
+def part_1():
+    return 
+
+def part_2():
+    return
+
+startTime = time.time()
+print("THE RESULT OF PART 1: " + str(part_1()))
+print("THE RESULT OF PART 2: " + str(part_2()))
+finalTime = time.time() - startTime
+print("THE PROGRAM RAN FOR " + str(finalTime) + " SECONDS")
